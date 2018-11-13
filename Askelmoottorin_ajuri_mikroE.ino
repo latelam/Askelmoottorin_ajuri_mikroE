656469a513234615dc5bb63affec50e377ab66cf
Lauri Marttila <lauri.marttila@ictpro.labra>
2018-11-13 15:08:16 +0200

    moottorin_nopeuden_optimointi

---
+++ Askelmoottorin_ajuri_mikroE.ino
@@ -0,0 +1,72 @@
+const int MS1 = 12;
+const int MS2 = 11;
+const int ENAB = 10;
+const int STEP = 9;
+const int SLEEP = 8;
+const int RESET = 7;
+const int DIR = 5;
+
+const int MP = HIGH;//Myötäpäivään
+const int VP = LOW;//Vastapäivään
+
+int suunta = MP;
+int viive = 500;
+
+void setup() {
+
+  pinMode(MS1, OUTPUT);
+  pinMode(MS2, OUTPUT);
+  pinMode(ENAB, OUTPUT);
+  pinMode(STEP, OUTPUT);
+  pinMode(SLEEP, OUTPUT);
+  pinMode(RESET, OUTPUT);
+  pinMode(DIR, OUTPUT);
+
+//HALF STEP VAUHTI
+  digitalWrite(MS1, LOW);
+  digitalWrite(MS2, LOW);
+//Lähdöt aktiiviset  
+  digitalWrite(ENAB, LOW);
+//Alas odottamaan ekaa käskyä  
+  digitalWrite(STEP, LOW);
+//Herätys  
+  digitalWrite(SLEEP,HIGH);
+//Laite päälle  
+  digitalWrite(RESET, HIGH);  
+  digitalWrite(DIR, suunta);
+
+  Serial.begin(9600);
+}
+
+int tUp=1000;
+int tDn=1000;
+
+void step(){
+  digitalWrite(STEP, HIGH);
+  delayMicroseconds(tUp);
+  digitalWrite(STEP, LOW);
+  delayMicroseconds(tDn);
+  
+}
+
+
+
+void loop() {
+  if(Serial.available()){
+    int x=Serial.read();
+    switch(x){
+      case 'q':tUp+=10;break;
+      case 'a':tUp-=10;break;
+      case 'w':tDn+=10;break;
+      case 's':tDn-=10;break;
+      case 'l':Serial.print(tUp);Serial.print(" ");Serial.println(tDn);break;
+    }
+  }
+  step();
+ 
+  
+  
+
+  
+}
+
